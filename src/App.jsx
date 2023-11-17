8891cf67250e91ed73d4929657d20240d738c2a1
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2023-11-17 15:34:51 -0500

    TabButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 import {CORE_CONCEPTS} from './data.js';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept';
 import TabButton from './components/TabButton';
 
 function App() {
+
+  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+
+  function handleSelect(selectedButton){
+    // selectedButton => 'Components', 'jsx', 'props', 'state'    
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
+  console.log('App component executing');
+
   return (
     <div>
       <Header />
@@ -23,11 +36,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Componentes</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={ () => handleSelect('components')}>Componentes</TabButton>
+            <TabButton onSelect={ () => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={ () => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={ () => handleSelect('state')}>State</TabButton>
           </menu>
+          {selectedTopic}
         </section>
       </main>
     </div>
