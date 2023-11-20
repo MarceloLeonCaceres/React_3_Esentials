171804480b28a64903767ab14a1dd38ce431e945
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2023-11-20 14:29:10 -0500

    63 Splitting components by feature and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,8 @@
-import { useState, Fragment } from 'react';
-
-import {CORE_CONCEPTS} from './data.js';
 import Header from './components/Header/Header';
-import CoreConcept from './components/CoreConcept';
-import TabButton from './components/TabButton';
-import { EXAMPLES } from './data.js';
+import CoreConcepts from './components/CoreConcepts';
+import Examples from './components/Examples';
 
 function App() {
-
-  const [ selectedTopic, setSelectedTopic ] = useState();
-
-  function handleSelect(selectedButton){
-    // selectedButton => 'Components', 'jsx', 'props', 'state'    
-    setSelectedTopic(selectedButton);
-    console.log(selectedTopic);
-  }
-
-  console.log('App component executing');
-
-  let tabContent = <p>Please select a topic</p>;
-
-  if(selectedTopic){
-    tabContent = ( 
-      <div id='tab-content'>
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div> 
-    );
-  }
 
   return (
     // Ni siquiera se escribe la palabra div
@@ -38,28 +10,8 @@ function App() {
     <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            { CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={selectedTopic === 'components'} 
-              onSelect={ () => handleSelect('components')}>Componentes</TabButton>
-            <TabButton isSelected={selectedTopic === 'jsx'} 
-              onSelect={ () => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === 'props'}  
-              onSelect={ () => handleSelect('props')}>Props</TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} 
-              onSelect={ () => handleSelect('state')}>State</TabButton>
-          </menu>
-            {tabContent}
-          </section>
+        <CoreConcepts />
+        <Examples />
       </main>
     </>
   );
